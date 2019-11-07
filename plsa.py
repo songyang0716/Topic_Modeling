1583260c807c80bd7fedc7f1cd188137c4bbcc04
songyang0716 <yangsong0716@yahoo.com>
2019-11-06 23:47:44 -0800

    update plsa

--- plsa.py
+++ plsa.py
@@ -36,5 +36,41 @@ for review in reviews:
     s = [stemmer.stem(word) for word in s if word.isalpha()]
     clean_reviews.append(s)
 
+unique_words = set([word for review in clean_reviews for word in review])
 
-def plsa(num_of_iterations):
+def plsa(clean_reviews, num_of_topics, num_of_iterations, num_of_unique_words):
+    ####################################################################################
+    ### clean_reviews: clean reviews that has been tokenized, store in a list        ###
+    ### num_of_topics: number of topics to generate                                  ###
+    ### number_of_iterations: collapsed gibbs sampling iterations                    ###
+    ####################################################################################
+
+    # words distribution in each topics
+    nwz = np.random.rand(num_of_unique_words, num_of_topics)
+    pwz = nwz/nwz.sum(axis=0,keepdims=1)
+    # the topic distribution for each document
+    nzd = np.random.rand(num_of_topics, len(clean_reviews))
+    pzd = nzd/nzd.sum(axis=0,keepdims=1)
+
+    pzwd = np.zeros((num_of_topics, num_of_unique_words, len(clean_reviews)))
+
+    for i in range(num_of_iterations):
+    	# E-step
+    		pwd = np.matmul(pwz, pzd)
+    		for j in range(num_of_unique_words):
+    			for k in range(len(clean_reviews)): 
+    				pzwd[:,j,k] = np.multiply(pwz[j,:], pzd[:,k]) / pwd[j,k]
+
+    	# M-step
+
+
+
+
+
+
+
+
+
+
+
+
